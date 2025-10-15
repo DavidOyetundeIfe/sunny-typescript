d87bae305ca8a6440f7def9250819e9e69d06bc6
DavidOyetundeIfe <oyetundedvd101@gmail.com>
2025-10-15 11:41:29 +0100

    first commit

---
+++ TSNC/index.ts
@@ -0,0 +1,23 @@
+let age = 30
+
+console.log(age)
+let isFictional: boolean
+
+age = 31
+firstName = 'david'
+isFictional = false
+
+let planet = 'jupiter'
+let moon = 1
+let isLarge = false
+
+planet = 'saturn'
+
+let something: null
+let anotherThing: undefined
+
+something = null
+anotherThing = undefined
+
+
+console.log("ni hao")
